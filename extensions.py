a53c7f58541adcc6c28cfaad51feafa3b978589d
alangraf <alan.graf@gmx.de>
2022-09-08 11:44:08 +0200

    changed mov to MOV

--- extensions.py
+++ extensions.py
@@ -1,7 +1,7 @@
 FILE_EXTENSIONS = {
     "pdf": ['pdf'],
     "shell": ['sh'],
-    "video": ['mp4', 'mov'],
+    "video": ['mp4', 'MOV'],
     "music": ['mp3'],
     "word": ['doc', 'docx'],
     "image": ['svg', 'webp', 'jpg', 'jpeg', 'png', 'heic', 'HEIC'],
