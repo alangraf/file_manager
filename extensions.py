0fca09206ddc4b52ae894cfc31e5127f98334af3
alangraf <alan.graf@gmx.de>
2022-09-08 11:41:18 +0200

    added mov extension

--- extensions.py
+++ extensions.py
@@ -1,7 +1,7 @@
 FILE_EXTENSIONS = {
     "pdf": ['pdf'],
     "shell": ['sh'],
-    "video": ['mp4'],
+    "video": ['mp4', 'mov'],
     "music": ['mp3'],
     "word": ['doc', 'docx'],
     "image": ['svg', 'webp', 'jpg', 'jpeg', 'png', 'heic', 'HEIC'],
